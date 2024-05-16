5d2b4786389e8ec03eb54458b6851bbc0fe16fec
olamideazeez <65969103+olamideazeez@users.noreply.github.com>
2024-05-16 20:20:23 +0100

    Add files via upload

---
+++ My project/javascript.js
@@ -0,0 +1,7 @@
+function showsidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+  }
+  function hidesidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'}
